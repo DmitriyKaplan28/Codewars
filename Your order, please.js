46feabbf32fb311ce14bbe527e96e20a0f31b09c
DmitriyKaplan28 <dsk007@mail.ru>
2022-08-24 18:00:08 +0300

    +2 fix

--- Your order, please.js
+++ Your order, please.js
@@ -1,13 +1,13 @@
-function order(words){
+function order(words) {
     let arr = words.split(' ')
     let r = []
 
-    arr.forEach(word=>{
+    arr.forEach(word => {
         let x = word.split('')
-        let num = x.find(el=>parseInt(el))
-        r.push([word,parseInt(num)])
+        let num = x.find(el => parseInt(el))
+        r.push([word, parseInt(num)])
     })
 
-    r.sort((a,b)=>a[1]-b[1]).map(x=> x.splice(1,1))
+    r.sort((a, b) => a[1] - b[1]).map(x => x.splice(1, 1))
     return r.join(' ')
 }
