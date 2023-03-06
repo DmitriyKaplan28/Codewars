8d9f9739dac5dbd56b96d962aed1c7ddc5ccb580
DmitriyKaplan28 <dsk007@mail.ru>
2023-03-06 22:28:30 +0300

    +1

--- Detect Pangram.js
+++ Detect Pangram.js
@@ -1,7 +1,7 @@
-function isPangram(string){
+function isPangram(string) {
     string = string.toLowerCase();
     return "abcdefghijklmnopqrstuvwxyz"
-        .split("").every(function(x){
+        .split("").every(function (x) {
             return string.indexOf(x) !== -1;
         });
 }
