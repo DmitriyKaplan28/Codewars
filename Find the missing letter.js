f05bdb664e6c1efa03602dd28f9989676b0531c1
DmitriyKaplan28 <dsk007@mail.ru>
2023-03-12 15:10:48 +0300

    +1

---
+++ Find the missing letter.js
@@ -0,0 +1,9 @@
+function findMissingLetter(array) {
+    let first = array[0].charCodeAt(0)
+    for (let i = 0; i < array.length; i++) {
+        if (first + i !== array[i].charCodeAt(0))
+            return String.fromCharCode(first + i)
+    }
+}
+
+console.log(findMissingLetter(['a','c', 'd']))
--- Moving Zeros To The End.js
+++ Moving Zeros To The End.js
@@ -1,8 +1,9 @@
 function moveZeros(arr) {
-    for(var i = arr.length; i--;) {
-        if(arr[i]===0) {
+    for (var i = arr.length; i--;) {
+        if (arr[i] === 0) {
             arr.splice(i, 1);
             arr.push(0);
         }
-    } return arr;
+    }
+    return arr;
 }
