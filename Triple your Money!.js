e45b99c1c15d813771938e61f48c543710982710
DmitriyKaplan28 <dsk007@mail.ru>
2023-04-06 17:29:03 +0300

    +1

--- Triple your Money!.js
+++ Triple your Money!.js
@@ -1,8 +1,8 @@
 function f(n) {
-    if(n==0) return 0;
+    if (n == 0) return 0;
     let str = n.toString(3)
-    if(str[0] == '1')
-        str = '2' +  str.slice(1)
+    if (str[0] == '1')
+        str = '2' + str.slice(1)
     else
         str = '1' + str.slice(1) + '0'
     return parseInt(str, 3);
