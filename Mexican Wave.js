2d4433178652159333872d0c4835a75b1ff14ce0
DmitriyKaplan28 <dsk007@mail.ru>
2023-06-07 23:17:50 +0300

    +1

---
+++ Mexican Wave.js
@@ -0,0 +1,13 @@
+function wave(string){
+    let waveArray = [];
+    for (let i = 0; i < string.length; i++) {
+        let letter = string[i]
+        if (letter === " ") {
+        } else {
+            waveArray.push(string.slice(0, i) + letter.toUpperCase() + string.slice(i + 1))
+        }
+
+    }
+
+    return waveArray;
+}
