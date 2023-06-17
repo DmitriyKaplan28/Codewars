a64c468d4a7c0a9d23123cd4f32c6cbe529d0d81
DmitriyKaplan28 <dsk007@mail.ru>
2023-06-17 15:03:15 +0300

    +1

--- Are they the same.js
+++ Are they the same.js
@@ -1,3 +1,3 @@
-function comp(array1, array2){
-    return !!array1 && !!array2 && array1.map(x=>x*x).sort().join() == array2.sort().join()
+function comp(array1, array2) {
+    return !!array1 && !!array2 && array1.map(x => x * x).sort().join() == array2.sort().join()
 }
