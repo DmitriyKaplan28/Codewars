053d54219501c87fd09e6a90964d8b534149220e
DmitriyKaplan28 <dsk007@mail.ru>
2023-07-09 02:02:12 +0300

    +1

---
+++ Make the Deadfish Swim.js
@@ -0,0 +1,20 @@
+function parse(data) {
+    let v = 0, ret = []
+    for (var c of data) {
+        switch (c) {
+            case 'i' :
+                v++;
+                break;
+            case 'd' :
+                v--;
+                break;
+            case 's' :
+                v = v * v;
+                break;
+            case 'o' :
+                ret.push(v);
+                break;
+        }
+    }
+    return ret;
+}
