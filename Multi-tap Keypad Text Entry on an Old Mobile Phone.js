80db4667751b2ed805df728357902125531f0151
DmitriyKaplan28 <dsk007@mail.ru>
2023-10-09 17:28:16 +0300

    +1

---
+++ Multi-tap Keypad Text Entry on an Old Mobile Phone.js
@@ -0,0 +1,2 @@
+const presses = phrase =>
+    [...phrase.toLowerCase()].reduce((pre, val) => pre + [`1adgjmptw* #`, `0behknqux`, `cfilorvy`, `234568sz`, `79`].findIndex(v => v.includes(val)) + 1, 0);
