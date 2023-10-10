b2cfc2253432f461ecbbefad7bea9aedca9d13fd
DmitriyKaplan28 <dsk007@mail.ru>
2023-10-10 21:16:10 +0300

    +1

---
+++ Character with longest consecutive repetition.js
@@ -0,0 +1,21 @@
+function longestRepetition(s) {
+    let count = 0;
+    let prevLetter = '';
+
+    return s.toLowerCase().split('').reduce((acc, curr) => {
+        if (curr === prevLetter) {
+            count++;
+        }
+        else {
+            count = 1;
+        }
+
+        if (count > acc[1]) {
+            acc[1] = count;
+            acc[0] = curr;
+        }
+
+        prevLetter = curr;
+        return acc;
+    }, ['', 0]);
+}
