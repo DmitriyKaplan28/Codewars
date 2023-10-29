58db5a855db662c19ef5c1c391726acea41f2b91
DmitriyKaplan28 <dsk007@mail.ru>
2023-10-29 20:22:10 +0300

    +1

---
+++ String transformer.js
@@ -0,0 +1,10 @@
+function stringTransformer(str) {
+    const allUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return str
+        .split('')
+        .map(c => allUpperCase.indexOf(c) === -1 ? c.toUpperCase() : c.toLowerCase())
+        .join('')
+        .split(' ')
+        .reverse()
+        .join(' ')
+}
